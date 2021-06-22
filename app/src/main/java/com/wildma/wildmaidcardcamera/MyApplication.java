08d6a8416c3eca97242b50352a14dc90656aa769
cuongnv243 <cuongnv243@ghtk.vn>
2021-06-22 14:06:51 +0700

    Init commit

---
+++ app/src/main/java/com/wildma/wildmaidcardcamera/MyApplication.java
@@ -0,0 +1,17 @@
+package com.wildma.wildmaidcardcamera;
+
+import android.app.Application;
+
+/**
+ * Author       wildma
+ * Github       https://github.com/wildma
+ * Date         2019/04/28
+ * Desc	        ${MyApplication}
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
